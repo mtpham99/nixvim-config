8c7affa251fc5cbf3c49e77bb8936286e4b1b28a
Matthew T. Pham (Github.com) <mtpham.github@quantamail.net>
2024-11-21 21:54:34 -0600

    added autocmd for trimming trailing whitespace

--- config/autocommands.nix
+++ config/autocommands.nix
@@ -6,7 +6,7 @@
   # https://nix-community.github.io/nixvim/NeovimOptions/autoGroups/index.html
   autoGroups = {
     highlight-yank = {
-      clear= true;
+      clear = true;
     };
   };
 
@@ -19,6 +19,21 @@
       callback.__raw = ''
         function()
           vim.highlight.on_yank()
+        end
+      '';
+    }
+
+    # trim trailing white space
+    # credit to: https://vi.stackexchange.com/a/41388
+    {
+      event = [ "BufWritePre" ];
+      desc = "Trim trailing whitespace";
+      pattern = [ "*" ];
+      callback.__raw = ''
+        function()
+          local save_cursor = vim.fn.getpos(".")
+          pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+          vim.fn.setpos(".", save_cursor)
         end
       '';
     }
