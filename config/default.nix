f161343a54915dd7c13f4033e5071d30430dced9
Matthew T. Pham (Github.com) <mtpham.github@quantamail.net>
2025-07-22 03:07:09 -0500

    config update: performance
    - lazy load plugin (lz-n)
    - byte compile lua
    - combined plugin pack

--- config/default.nix
+++ config/default.nix
@@ -15,4 +15,24 @@
   # extra lua config added at top and bottom of init.lua
   extraConfigLuaPre = '''';
   extraConfigLuaPost = '''';
+
+  # lz-n plugin for lazy loading other plugins
+  # see: https://nix-community.github.io/nixvim/plugins/lz-n/index.html
+  #      https://github.com/nvim-neorocks/lz.n
+  plugins.lz-n.enable = true;
+
+  performance = {
+    # see: https://nix-community.github.io/nixvim/performance/byteCompileLua.html
+    byteCompileLua = {
+      enable = true;
+      configs = true;
+      initLua = true;
+      luaLib = true;
+      nvimRuntime = true;
+      plugins = true;
+    };
+
+    # see: https://nix-community.github.io/nixvim/performance/combinePlugins.html
+    combinePlugins.enable = true;
+  };
 }
